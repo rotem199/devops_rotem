b66ce8f46e03e5fad9a3f10692635c07afe5ec45
rotem199 <rotemg44@gmail.com>
2022-11-13 19:51:59 +0200

    Rotem Zaguri 2 print modifide

--- RotemZaguri.java
+++ RotemZaguri.java
@@ -1,7 +1,7 @@
-public class RotemZaguri {
+public class RotemZaguri{
    public static void main(String[] args){
        for (int i=1;i<=10;i++){
-	   System.out.println("Rotem Zguri"+i);
+	   System.out.println("Rotem Zguri !!!!!"+i);
 	   }
    }
 }
